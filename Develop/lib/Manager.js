0ab520a4fb34ef08c085e0d352a046cc5ac3e0e8
Jon Christie <jonpchristie@gmail.com>
2020-05-09 21:21:57 -0400

    start

---
+++ Develop/lib/Manager.js
@@ -0,0 +1,6 @@
+// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee.js");
+
+class Manager extends Employee {
+    constructor(name, id, email, imageUrl, officeNumber)
+}
