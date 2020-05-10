89cdc9fbe403b19e1d5f05798b0bdea3089c1ec8
Jon Christie <jonpchristie@gmail.com>
2020-05-09 21:51:09 -0400

    Test suites 1/4, tests 8/17 after building employee.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,32 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email, imageUrl) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.imageUrl = imageUrl;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getImageUrl() {
+        return this.imageUrl;
+    }
+
+    getRole() {
+        return "Employee";
+    }
+}
+
+
+module.exports = Employee;
